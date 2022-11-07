580cc5fdc4ae208898f196b3cbc0181930845452
shafik720 <shafikrasel5@gmail.com>
2022-11-07 20:35:15 +0600

    working on google sign in

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import app from './firebaseInit';
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+function handleAuthentication(){
+  console.log('go');
+}
 
 function App() {
   return (
     <div className="App">
-      
+      <button onClick={handleAuthentication}>Google Sign In</button>
     </div>
   );
 }
