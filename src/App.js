e150542c77e11ef747920677741d95fb296d0365
shafik720 <shafikrasel5@gmail.com>
2022-11-08 14:33:11 +0600

    showing user details

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import app from './firebaseInit';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from 'react';
 
 
 
 function App() {
+  const [user, setUser] = useState([]);
 
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
@@ -14,6 +16,7 @@ function App() {
     signInWithPopup(auth, provider)
     .then(result=>{
       console.log(result);
+      setUser(result.user)
     }).catch(error=>{
       console.log(error)
     })
@@ -22,6 +25,7 @@ function App() {
     <div className="App">
       <h2>Hello</h2>
       <button onClick={handleAuthentication}>Sign in with google</button>
+      <h2>Name : {user.displayName} </h2>
     </div>
   );
 }
